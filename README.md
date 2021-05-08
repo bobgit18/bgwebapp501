3b2eff6ae7fd5d73420850061808dbb3d3fc9bfa
bobgit18 <bobcareers2018@outlook.com>
2021-05-08 15:39:42 +0100

    added webapp project

--- README.md
+++
@@ -1,1 +0,0 @@
-# bgwebapp501
---
+++ bgwebapp501/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bgwebapp501.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
